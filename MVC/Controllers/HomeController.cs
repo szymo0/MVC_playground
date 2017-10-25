3f415d42856d88450ab4755c0840499937a01266
Nowak, Julita <julita.doroszko@hpe.com>
2017-10-25 13:23:20 +0200

    add playground

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ShowDriver()
+        {
+            var driver = new Driver
+            {
+                Name = "szumon",
+                Surname = "szczypański",
+                Pesel = "870601"
+            };
+            
+            return View(driver);
+        }
+    }
+}
---
+++ MVC/Models/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class Driver
+    {
+        public Driver()
+        {
+            
+        }
+        [DisplayName("Imię")]
+        public string Name { get; set; }
+        [DisplayName("Nazwisko")]
+        public string Surname { get; set; }
+        [DisplayName("PESEL")]
+        [RegularExpression(@"\d{1,11}",ErrorMessage = "Błędny format peslu")]
+        public string Pesel { get; set; }
+        [DisplayName("Żywy?!")]
+        public bool IsAlive { get; set; }
+    }
+}
